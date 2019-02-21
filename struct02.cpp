4bcdce7127143145ec4de09691285caae26db96e
kunchaiCH <gun1one2@gmail.com>
2019-02-22 02:18:03 +0700

    random  name student

--- struct02.cpp
+++ struct02.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 using namespace std;
 
-main(){
+struct Student{
+    char name[16];
 
+    Student(){
+        name[0] = '\0';
+        // langth name 4 - 15
+        // random char 65 - 90
+        // strcat
+        int len = rand()%12 + 4; // 4 - 15
+        int i;
+        for (i = 0; i < len; i++){
+             char rc = rand()%26 + 65;
+             name[i] = rc;             
+        }
+        name[i] = '\0';
+    }
+};
+
+main(){
+    srand(time(NULL));
+    Student s1;
+    cout << "name : " << s1.name << endl;
     return 0;
 }
