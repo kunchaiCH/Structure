e71b5a4ac6ad7e332fe41bea7924cda6c1203c9a
kunchaiCH <gun1one2@gmail.com>
2019-02-22 01:19:00 +0700

    set age, id and gpa

--- struct01.cpp
+++ struct01.cpp
@@ -1,22 +1,30 @@
-#include <iostream>
+#include <iostream>             // std::cout
 #include <stdio.h>
-using namespace std;
+#include <string.h>             // strcpy
+using namespace std;            // std
 
-struct Student{
+struct Student{                 // Struct name: Student
     char id[9];
     float gpa;
     int age;
     char name[30];
 
-    Student(){              // constructor function
+    Student(){                  // constructor function
         id[0] = '\0';
         gpa = -99;
         age = -99;
         name[0] = '\0';
     }
+
+    void Birth(int years){      // function Birth
+        age = 2562 - years;
+    }
 };
 main (){
     Student s1;
-    cout << "id =" << s1.id <<", gpa" << s1.gpa << endl; // Test constructor function
-    cout << "Hello" << endl;
+    cout << "id = " << s1.id <<", gpa = " << s1.gpa << ", age = " << s1.age << endl; // Test constructor function
+    s1.Birth(2541);              // call the function Birth
+    strcpy(s1.id, "603110");     // strcpy // include <string.h>
+    s1.gpa = 3.50;               // set gpa
+    cout << "id = " << s1.id <<", gpa = " << s1.gpa << ", age = " << s1.age << endl; //  After set
 }
