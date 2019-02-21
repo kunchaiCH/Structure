670b1f7100c49d78f68bdeac5a246f5752d4b441
kunchaiCH <gun1one2@gmail.com>
2019-02-21 22:34:05 +0700

    Test constructor

--- struct01.cpp
+++ struct01.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 #include <stdio.h>
 using namespace std;
+
+struct Student{
+    char id[9];
+    float gpa;
+    int age;
+    char name[30];
+
+    Student(){
+        id[0] = '\0';
+        gpa = -99;
+        age = -99;
+        name[0] = '\0';
+    }
+};
 main (){
-    cout << "Test print" << endl;
-    cout << "Hello Structure" << endl;
+    Student s1;
+    cout << "id =" << s1.id <<", gpa" << s1.gpa << endl;
 }
